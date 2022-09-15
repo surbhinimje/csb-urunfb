0103f022e89ef23f08aa29b5d2685e549c1c3e4a
Code Sandbox <hello@codesandbox.io>
2022-09-15 18:52:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.db0808152776479f75807548bc893931.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1404e1bbae4c717f546cdcc87181d9",
+    "url": "https://surbhinimje.github.io/csb-urunfb/index.html"
+  },
+  {
+    "revision": "bb2a6d8764d8975888d4",
+    "url": "https://surbhinimje.github.io/csb-urunfb/static/js/2.2d5bdb4a.chunk.js"
+  },
+  {
+    "revision": "073b6b618b179ff06752",
+    "url": "https://surbhinimje.github.io/csb-urunfb/static/js/main.4a03dcd2.chunk.js"
+  },
+  {
+    "revision": "182be877629a2df84b06",
+    "url": "https://surbhinimje.github.io/csb-urunfb/static/js/runtime-main.7c7f77a1.js"
+  }
+]);
